5ffd11b9132553cbbfd5149c49780d9dd2a1ba05
Jonathan Prince <w0270525@nscc.ca>
2015-04-29 15:35:13 -0300

    Changed to CMOD ch 2 pre made files.

--- roguelike/Assets/Scripts/MovingObject.cs
+++ roguelike/Assets/Scripts/MovingObject.cs
@@ -1,15 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovingObject : MonoBehaviour {
+public abstract class MovingObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float moveTime = 0.1f;
+    //layer some prefabs are on.
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+
+    }
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        //Store start position to move from, based on objects current transform position.
+        Vector2 start = transform.position;
+
+        // Calculate end position based on the direction parameters passed in when calling Move.
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        //Disable the boxCollider so that linecast doesn't hit this object's own collider.
+        boxCollider.enabled = false;
+
+        //Cast a line from start point to end point checking collision on blockingLayer.
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+
+        //Re-enable boxCollider after linecast
+        boxCollider.enabled = true;
+
+        //Check if anything was hit
+        if (hit.transform == null)
+        {
+            //If nothing was hit, start SmoothMovement co-routine passing in the Vector2 end as destination
+            StartCoroutine(SmoothMovement(end));
+
+            //Return true to say that Move was successful
+            return true;
+        }
+
+        //If something was hit, return false, Move was unsuccesful.
+        return false;
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while(sqrRemainingDistance> float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove <T> (int xDir, int yDir)
+        where T: Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+        if (hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+    protected abstract void OnCantMove<T>(T component)
+        where T : Component;
+
 }
