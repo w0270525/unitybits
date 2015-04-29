53520beb134df5a714684c668f24d116f6d68f6b
Jonathan Prince <w0270525@nscc.ca>
2015-04-29 00:41:58 -0300

    Touch screen parts implimented. not tested yet.

--- roguelike/Assets/Scripts/Player.cs
+++ roguelike/Assets/Scripts/Player.cs
@@ -22,6 +22,8 @@ public class Player : MovingObject
     private Animator animator;
     private int food;
 
+    //mobile line
+    private Vector2 touchOrigin = -Vector2.one;
 
     // Use this for initialization
     protected override void Start()
@@ -43,11 +45,14 @@ public class Player : MovingObject
     // Update is called once per frame
     void Update()
     {
+
         if (!GameManager.instance.playersTurn) return;
 
         int horizontal = 0;
         int vertical = 0;
 
+
+      #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
         horizontal = (int)Input.GetAxisRaw("Horizontal");
         vertical = (int)Input.GetAxisRaw("Vertical");
 
@@ -55,6 +60,32 @@ public class Player : MovingObject
         if (horizontal != 0)
             vertical = 0;
 
+    #else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                    horizontal = x > 0 ? 1 : -1;
+                else
+                    vertical = y > 0 ? 1 : -1;
+
+
+
+            }
+        }
+#endif
         if (horizontal != 0 || vertical != 0)
             AttemptMove<Wall>(horizontal, vertical);
 
