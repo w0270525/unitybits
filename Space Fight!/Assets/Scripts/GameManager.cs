66a905425055303d5589cfd4d508a7e205bae089
Jonathan Prince <w0270525@nscc.ca>
2015-05-26 13:37:33 -0300

    moved some items

---
+++ Space Fight!/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float startWait = 3f;
+    public GameObject[] SpawnPoints;
+
+    public int Score = 0;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    SpawnPoints = GameObject.FindGameObjectsWithTag("EnemySpawn");
+
+	    StartCoroutine(SpawnWaves());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            
+        }
+    }
+
+
+}
+
