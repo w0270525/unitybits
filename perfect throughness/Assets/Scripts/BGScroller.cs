6b840d840232534726b555d32967184117f1b1a6
Jonathan Prince <prince.jon@gmail.com>
2016-04-22 19:07:01 -0300

    added bgscrolling

---
+++ perfect throughness/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+
+
+    public float scrollSpeed; //speed in which the background will move.
+    public float tileSizeY; //size of the background sprite.
+
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        startPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+        transform.position = startPosition + Vector3.up * newPosition;
+    }
+}
