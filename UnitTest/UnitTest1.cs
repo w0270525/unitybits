7fa6355384913529eace50e1b408d30210b6d1ad
Jonathan Prince <prince.jon@gmail.com>
2016-08-08 11:39:18 -0300

    comment added to restarter.cs

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using UnityEngine;
+//using PerfectThroughness;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Main()
+        {
+            Motorcycle moto = new Motorcycle();
+            Assert.IsNotNull(moto.speed);
+        }
+    }
+}
---
+++ perfect throughness/Assets/Scripts/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Assets.Scripts
+{
+    class Class1
+    {
+    }
+}
